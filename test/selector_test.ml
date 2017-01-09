656c810dd69e199418d6c2cc1d2b875609832acf
Ted Kornish <ted.kornish+github@gmail.com>
2017-01-08 23:04:47 -0800

    Move selector test to be table-based as well

--- test/selector_test.ml
+++ test/selector_test.ml
@@ -2,35 +2,20 @@ open OUnit2
 open Grammar
 open Utils
 
-let test1 ctx = assert_equal (exp_output (Exp (None, [])) "{}") "{}"
-let test2 ctx = assert_equal
-    "{\"hello\":\"world\"}"
-    (exp_output
-       (Exp (None, [Selector "hello"]))
-       "{\"x\": \"y\", \"hello\": \"world\"}")
-let test3 ctx =
-  assert_equal
-    "{\"hello\":\"world\"}"
-    (
-      exp_output
-        (Exp (None, [Selector "hello"; Selector "merp"])) "{\"x\": \"y\", \"hello\": \"world\"}"
-    )
-let test4 ctx =
-  assert_equal
-    "{\"hello\":\"world\",\"x\":\"y\",\"a\":8}"
-    (
-      exp_output
-        (Exp (None, [Selector "hello"; Selector "x"; Selector "a"]))
-        "{\"a\": 8, \"x\": \"y\", \"hello\": \"world\"}"
-    )
-let test5 ctx = assert_equal
-    ~printer:(fun x -> x)
-    "{\"myKey\":\"myVal\"}"
-    (exp_output (Exp (None, [Selector "myKey"])) "{\"myKey\": \"myVal\", \"Merp\": 19}")
-let suite = "selector integration test suite" >::: [
-    "with empty object and no predicate and no selectors" >:: test1;
-    "with selectors as a subset of object keys" >:: test2;
-    "with selector on a key not in the object" >:: test3;
-    "with selector in an order different from the object's keys" >:: test4;
-    "with selector with capital letters and values" >:: test5
-  ]
+let cases = [
+  ("with empty object and no selectors", "{}", [], "{}");
+  ("with selectors as a subset of object keys", "{\"hello\":\"world\"}", ["hello"], "{\"x\": \"y\", \"hello\": \"world\"}");
+  ("with selector on a key not in the object", "{\"hello\":\"world\"}", ["hello"; "merp"], "{\"x\": \"y\", \"hello\": \"world\"}");
+  ("with selectors in an order different from the object's keys", "{\"hello\":\"world\",\"x\":\"y\",\"a\":8}", ["hello"; "x"; "a"], "{\"a\": 8, \"x\": \"y\", \"hello\": \"world\"}");
+  ("with selector with capital letters and values", "{\"myKey\":\"myVal\"}", ["myKey"], "{\"myKey\": \"myVal\", \"Merp\": 19}");
+]
+
+let suite =
+  "selector integration test suite" >::: List.map (fun (reason, expected, selector_strings, raw) ->
+      let to_selector s = Selector s in
+      reason >:: (fun ctxt -> assert_equal ~ctxt:ctxt
+                     expected
+                     (exp_output (Exp (
+                          None,
+                          List.map to_selector selector_strings)) raw))
+    ) cases
